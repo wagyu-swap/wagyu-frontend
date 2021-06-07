48d9bfd6929e367c26613a8f832e26288218e18b
Angel <rmsangel316@gmail.com>
2021-06-07 13:43:18 +0300

    issue #5 - update title and icon  * update title and icon  * change Syrup to Sauce  * update variable names

--- src/config/constants/meta.ts
+++ src/config/constants/meta.ts
@@ -5,7 +5,7 @@ export const DEFAULT_META: PageMeta = {
   title: 'WagyuSwap',
   description:
     'The most popular AMM on BSC by user count! Earn CAKE through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by WagyuSwap), NFTs, and more, on a platform you can trust.',
-  image: 'https://WagyuSwap.finance/images/hero.png',
+  image: 'https://wagyuswap.finance/images/hero.png',
 }
 
 export const getCustomMeta = (path: string, t: ContextApi['t']): PageMeta => {
